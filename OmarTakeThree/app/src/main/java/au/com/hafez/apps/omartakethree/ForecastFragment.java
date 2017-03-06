02ad26b46d96346ffcdf1d45eae26cd5a8197413
omarau <omarau@users.noreply.github.com>
2017-03-05 23:38:32 -0500

    work in progress. Added call to internet to get back data and a menu to trigger it.

---
+++ OmarTakeThree/app/src/main/java/au/com/hafez/apps/omartakethree/ForecastFragment.java
@@ -0,0 +1,219 @@
+package au.com.hafez.apps.omartakethree;
+
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ForecastFragment extends Fragment {
+
+    ArrayAdapter<String> mForecastAdapter;
+
+    public ForecastFragment() {
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+//        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.forecastfragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_refresh) {
+            new FetchWeatherTask().execute("10028");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Create some dummy data for the ListView.  Here's a sample weekly forecast
+        String[] data = {
+                "Mon 6/23 - Sunny - 31/17",
+                "Tue 6/24 - Foggy - 21/8",
+                "Wed 6/25 - Cloudy - 22/17",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Sun 6/29 - Sunny - 20/7"
+        };
+
+        // String forecastJson = getWeatherData();
+
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+
+        mForecastAdapter = new ArrayAdapter<String>(
+                getActivity(), // this fragment's parent activity
+                R.layout.list_item_forecast, // id of list item layout
+                R.id.list_item_forecast_textview, // id of text view to populate
+                weekForecast); // data
+
+        // inflating loads the xml and converts it into objects.
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ListView listView = (ListView)rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(mForecastAdapter);
+
+        return rootView;
+    }
+
+    private class FetchWeatherTask extends AsyncTask<String, Void, Void> {
+
+        private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
+
+        @Override
+        protected Void doInBackground(String... params) {
+            if (params.length == 0)
+                return null;
+
+            String postCode = params[0];
+            getWeatherData(postCode);
+            return null;
+        }
+
+        private String getWeatherData(String postCode) {
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String forecastJsonStr = null;
+
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are avaiable at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+
+                final String BASE_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?";
+                final String POSTCODE_PARAM = "zip";
+                final String FORMAT_PARAM = "mode";
+                final String UNITS_PARAM = "units";
+                final String APPID_PARAM = "appid";
+                final String DAYS_PARAM = "cnt";
+                // URL url = new URL(BASE_URL + "zip=10028,us&units=metric&mode=json&appid=7a4d7085838ea084dd111481d105232d&cnt=7");
+                Uri builtUri = Uri.parse(BASE_URL).buildUpon()
+                        .appendQueryParameter(POSTCODE_PARAM, postCode)
+                        .appendQueryParameter(UNITS_PARAM, "metric")
+                        .appendQueryParameter(FORMAT_PARAM, "json")
+                        .appendQueryParameter(APPID_PARAM, "7a4d7085838ea084dd111481d105232d")
+                        .appendQueryParameter(DAYS_PARAM, "7")
+                        .build();
+
+                Log.i(LOG_TAG, builtUri.toString());
+                URL url = new URL( builtUri.toString());
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+
+                forecastJsonStr = buffer.toString();
+                Log.i(LOG_TAG, forecastJsonStr);
+
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally{
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+
+            return forecastJsonStr;
+        }
+    }
+}
+
+
+
+//
+//private class DownloadFilesTask extends AsyncTask<URL, Integer, Long> {
+//    protected Long doInBackground(URL... urls) {
+//        int count = urls.length;
+//        long totalSize = 0;
+//        for (int i = 0; i < count; i++) {
+//            totalSize += Downloader.downloadFile(urls[i]);
+//            publishProgress((int) ((i / (float) count) * 100));
+//            // Escape early if cancel() is called
+//            if (isCancelled()) break;
+//        }
+//        return totalSize;
+//    }
+//
+//    protected void onProgressUpdate(Integer... progress) {
+//        setProgressPercent(progress[0]);
+//    }
+//
+//    protected void onPostExecute(Long result) {
+//        showDialog("Downloaded " + result + " bytes");
+//    }
+//}
